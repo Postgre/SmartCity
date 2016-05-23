56dba1eead4a34fc2a4586a543f2632291894997
feng0413 <041332feng@163.com>
2016-05-23 15:48:46 +0800

    local changes

---
+++ http/src/main/java/com/smartcity/http/service/UploadService.java
@@ -0,0 +1,12 @@
+package com.smartcity.http.service;
+
+/**
+ * 在此写类用途
+ *
+ * @version V2.0
+ * @FileName: UploadService.java
+ * @author: fengliang
+ * @date: 2016-05-23 15:47
+ */
+public interface UploadService {
+}
